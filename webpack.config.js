594827d1a8127d597098e72ebc386f808dea7acd
dbb1703 <dbb1703@gmail.com>
2024-11-22 15:12:37 +0300

    Изменяет настройку webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,61 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// Импортируем плагин
 
-const devServer = (isDev) => !isDev ? {} : {
+let mode = 'development';
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+  target = 'browserslist';
+}
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: './src/index.html',
+  }),
+  new MiniCssExtractPlugin({
+    filename: '[name].[contenthash].css', // Формат имени файла
+  }), // Добавляем в список плагинов
+];
+
+module.exports = {
+  mode,
+  plugins, // Сокращенная запись plugins: plugins в ES6+
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
   devServer: {
     open: true,
     hot: true,
-    port: 8080,
-  }
-}
+    watchFiles: path.join(__dirname, 'src')
+  },
 
-module.exports = ({develop}) => ({
-  mode: develop ? 'development' : 'production',
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000
-  },
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: './styles/style.css'
-    })
-  ],
   module: {
     rules: [
+      { test: /\.(html)$/, use: ['html-loader'] },
       {
-        test: /\.(png|svg|jpg|jpeg)$/i,
+        test: /\.(s[ac]|c)ss$/i, // /\.(le|c)ss$/i если вы используете less
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      }, // Добавляем загрузчики стилей
+      {
+        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+        type: mode === 'production' ? 'asset' : 'asset/resource', // В продакшен режиме
+        // изображения размером до 8кб будут инлайнится в код
+        // В режиме разработки все изображения будут помещаться в dist/assets
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
-      {
-        test: /\.css$/i,
-        use: [
-          MiniCssExtractPlugin.loader, 'css-loader'
-        ]
-      },
-      {
-        test: /\.scss$/i,
-        use: [
-          MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      }
-    ]
-  },
-  ...devServer(develop),
-});
+    ],
+  }
+}
