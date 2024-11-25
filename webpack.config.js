aa5529105092d5d26f0d7755a38893c2384edcd2
dbb1703 <dbb1703@gmail.com>
2024-11-25 14:36:45 +0300

    Добавляет шрифты

--- webpack.config.js
+++ webpack.config.js
@@ -23,20 +23,22 @@ module.exports = {
   plugins, // Сокращенная запись plugins: plugins в ES6+
   entry: './src/index.js',
   devtool: 'source-map',
+  stats: 'errors-only',
   output: {
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[hash][ext][query]',
     clean: true,
   },
 
   devServer: {
     open: true,
     hot: true,
-    watchFiles: path.join(__dirname, 'src')
+    //watchFiles: path.join(__dirname, 'src')
   },
 
   module: {
     rules: [
-      { test: /\.(html)$/, use: ['html-loader'] },
+      {test: /\.(html)$/, use: ['html-loader']},
       {
         test: /\.(s[ac]|c)ss$/i, // /\.(le|c)ss$/i если вы используете less
         use: [
@@ -48,14 +50,22 @@ module.exports = {
       }, // Добавляем загрузчики стилей
       {
         test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
-        type: mode === 'production' ? 'asset' : 'asset/resource', // В продакшен режиме
-        // изображения размером до 8кб будут инлайнится в код
-        // В режиме разработки все изображения будут помещаться в dist/assets
+        type: mode === 'production' ? 'asset' : 'asset/resource',
       },
       {
         test: /\.(woff2?|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+          },
+        }
+      }
     ],
   }
 }
