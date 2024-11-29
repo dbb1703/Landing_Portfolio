8e0f81c733b23920e27e78c623d9b99dbf07ab2f
dbb1703 <dbb1703@gmail.com>
2024-11-29 16:16:19 +0300

    Дорабатывает webpack сборку

--- webpack.config.js
+++ webpack.config.js
@@ -3,69 +3,66 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // Импортируем плагин
 
-let mode = 'development';
-if (process.env.NODE_ENV === 'production') {
-  mode = 'production';
-  target = 'browserslist';
-}
+module.exports = (env, options) => {
+  let isProd = options.mode === 'production';
 
-const plugins = [
-  new HtmlWebpackPlugin({
-    template: './src/index.html',
-  }),
-  new MiniCssExtractPlugin({
-    filename: '[name].[contenthash].css', // Формат имени файла
-  }), // Добавляем в список плагинов
-];
+  return {
+    target: isProd ? 'browserslist' : 'web',
+    entry: './src/index.js',
+    devtool: 'source-map',
+    stats: 'errors-only',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      assetModuleFilename: 'assets/[hash][ext][query]',
+      clean: true,
+    },
 
-module.exports = {
-  mode,
-  plugins, // Сокращенная запись plugins: plugins в ES6+
-  entry: './src/index.js',
-  devtool: 'source-map',
-  stats: 'errors-only',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'assets/[hash][ext][query]',
-    clean: true,
-  },
+    devServer: {
+      open: true,
+      hot: true,
+      watchFiles: path.join(__dirname, 'src')
+    },
 
-  devServer: {
-    open: true,
-    hot: true,
-    watchFiles: path.join(__dirname, 'src')
-  },
-
-  module: {
-    rules: [
-      {test: /\.(html)$/, use: ['html-loader']},
-      {
-        test: /\.(s[ac]|c)ss$/i, // /\.(le|c)ss$/i если вы используете less
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
-        ],
-      }, // Добавляем загрузчики стилей
-      {
-        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
-        type: mode === 'production' ? 'asset' : 'asset/resource',
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            cacheDirectory: true,
-          },
+    module: {
+      rules: [
+        {test: /\.(html)$/, use: ['html-loader']},
+        {
+          test: /\.(s[ac]|c)ss$/i, // /\.(le|c)ss$/i если вы используете less
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'postcss-loader',
+            'sass-loader'
+          ],
+        }, // Добавляем загрузчики стилей
+        {
+          test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+          type: isProd ? 'asset/resource' : 'asset/inline',
+        },
+        {
+          test: /\.(woff2?|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+            },
+          }
         }
-      }
+      ],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css', // Формат имени файла
+      }), // Добавляем в список плагинов
     ],
-  }
+  };
 }
